4340361c2a43b49519e0a5f50bf9f6a784d124d7
sudoshreyansh <shreyanshjain14072002@gmail.com>
2022-05-07 13:44:52 +0530

    Revert "ROute setup"
    This reverts commit 2e8e983e00effe333c79950b2cd224348d2c97aa.

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
-// import logo from '../../assets/img/logo.svg';
-// import Greetings from '../../containers/Greetings/Greetings';
+import logo from '../../assets/img/logo.svg';
+import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
-import Navbar from './components/Navbar'
-import Login from './components/Login'
-import MemoryRouter from 'react-router-dom/MemoryRouter';
-import Route from 'react-router-dom/Route';
 
 const Popup = () => {
   return (
     <div className="App">
-      <Navbar />
-      <div className="containor">
-          <MemoryRouter>
-            <Route path='/' component={Login} />
-          </MemoryRouter>
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React!
+        </a>
+      </header>
     </div>
   );
 };
