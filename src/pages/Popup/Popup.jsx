ec072ba1f6e58075ce98f8b87f34639355d6400d
sudoshreyansh <shreyanshjain14072002@gmail.com>
2022-05-07 14:09:01 +0530

    added memory router

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar'
 import './Popup.css';
+import { MemoryRouter } from 'react-router-dom';
 
 const Popup = () => {
   return (
     <div className="App">
       <Navbar />
+      <MemoryRouter>
+          
+      </MemoryRouter>
     </div>
   );
 };
